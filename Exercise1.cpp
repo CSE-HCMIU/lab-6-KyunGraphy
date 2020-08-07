395632a20ad45f8f92d74c43d8d53d4560a56664
KyunGraphy <69331360+KyunGraphy@users.noreply.github.com>
2020-08-07 09:55:44 +0700

    Update Exercise1.cpp

--- Exercise1.cpp
+++ Exercise1.cpp
@@ -6,11 +6,148 @@
 |________________________________________________|
 */
 
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 
 int ex1(int testcase){
-	//your codes here
+	int n, t, d;
+	n = testcase;
+	t = 1000;
+	while (t){
+		d = n/t;
+		n %= t;
+		if (t > 10 || t == 1 || 10*d >= 20 && 10*d < 10){
+			switch (d){
+				case 1:{
+					cout <<"one ";
+					break;
+				}
+				case 2:{
+					cout <<"two ";
+					break;
+				}
+				case 3:{
+					cout <<"three ";
+					break;
+				}
+				case 4:{
+					cout <<"four ";
+					break;
+				}
+				case 5:{
+					cout <<"five ";
+					break;
+				}
+				case 6:{
+					cout <<"six ";
+					break;
+				}
+				case 7:{
+					cout <<"seven ";
+					break;
+				}
+				case 8:{
+					cout <<"eight ";
+					break;
+				}
+				case 9:{
+					cout <<"nine ";
+					break;
+				}
+			}
+		}
+		if (t == 1000 && d % t != 0){
+			cout <<"thousand ";
+		}
+		if (t == 100 && d % t != 0){
+			cout <<"hundred ";
+		}
+		if (t == 10){
+			switch (d){
+				case 1:{
+					int k;
+					k = n%t;
+					switch (k){
+						case 1:{
+							cout <<"and eleven ";
+							break;
+						}
+						case 2:{
+							cout <<"and twelve ";
+							break;
+						}
+						case 3:{
+							cout <<"and thirteen ";
+							break;
+						}
+						case 4:{
+							cout <<"and fourteen ";
+							break;
+						}
+						case 5:{
+							cout <<"and fifteen ";
+							break;
+						}
+						case 6:{
+							cout <<"and sixteen ";
+							break;
+						}
+						case 7:{
+							cout <<"and seventeen ";
+							break;
+						}
+						case 8:{
+							cout <<"and eighteen ";
+							break;
+						}
+						case 9:{
+							cout <<"andnineteen ";
+							break;
+						}
+						case 0:{
+							cout <<" ten ";
+							break;
+						}
+					}
+					break;
+				}
+				case 2:{
+					cout <<"twenty ";
+					break;
+				}
+				case 3:{
+					cout <<"thirty ";
+					break;
+				}
+				case 4:{
+					cout <<"forty ";
+					break;
+				}
+				case 5:{
+					cout <<"fifty ";
+					break;
+				}
+				case 6:{
+					cout <<"sixty ";
+					break;
+				}
+				case 7:{
+					cout <<"seventy ";
+					break;
+				}
+				case 8:{
+					cout <<"eighty ";
+					break;
+				}
+				case 9:{
+					cout <<"ninty ";
+					break;
+				}
+			}
+		}
+		t /= 10;
+	}
+}
 }
 
 int main(int argc, char *argv[]) {
